868420af9e99105e7ecc59a256b1ffb499df0730
Emman Oduor <wyllademanu003@gmail.com>
2025-11-27 13:24:40 +0300

    initial commit

--- index.js
+++ index.js
@@ -4,21 +4,24 @@ let featuredDrink = 'Strawberry Milkshake';
 
 // Step 2: Function and Block
 function addBurger() {
-    // Function-scoped variable
-    const newBurger = 'Flatburger';
-    
-    // Add to the burgers array
-    burgers.push(newBurger);
+  const newBurger = 'Flatburger';
+  burgers.push(newBurger);
 
-    // Block-scoped variables inside an if block
-    if (true) {
-        const anotherNewBurger = 'Maple Bacon Burger';
-        burgers.push(anotherNewBurger);
-    }
+  if (true) {
+    const anotherNewBurger = 'Maple Bacon Burger';
+    burgers.push(anotherNewBurger);
+  }
 }
 
 // Step 3: Function to change featured drink
 function changeFeaturedDrink() {
-    featuredDrink = 'The JavaShake';
-    
+  featuredDrink = 'The JavaShake';
 }
+// Step 4: Invoking Functions
+addBurger();
+changeFeaturedDrink();
+
+// Step 5: Output
+console.log('Burgers:', burgers);
+console.log('Featured Drink:', featuredDrink);
+
